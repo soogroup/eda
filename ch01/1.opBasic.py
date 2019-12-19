fc5795b10cca75ae66910358235b6164b0b1d0bc
Kim Hyoung-soo <soogroup@hanmail.net>
2019-12-19 15:15:55 +0900

    ch01-2

---
+++ ch01/1.opBasic.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 19 14:00:11 2019
+
+@author: edu
+"""
+
+a = 10
+#%%
+b = 20.0
+c = a + b
+#%%
+a
+#%%
+print(b)
+print(c)
+#%%
+age = 17
+h = 185.3
+print('Input name : ')
+name = input();
+print('your name is ' + name)
+print('your name is %s,%d,%f' % (name, age, h))
+
+#%%
+name = input('Input name : ')
+print('your name is ' + name)
+
+
+
+
+
+
+
+
+
+
+
---
+++ ch01/1opBasic.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 19 14:00:05 2019
+
+@author: edu
+"""
+
+a = 10
+#%%
+b = 20.0
+c = a + b
+#%%
+print(a)
+#%%
+print(b)
+print(c)
+#%%
+age = 17
+h = 185.3
+print('Input name :')
+name = input();
+print('your name %s' % name)
+print('your name is %s, %i, %f' % (name, age, h))
+#%%
+name = input('Input name :')
+print('your name is'+name)
